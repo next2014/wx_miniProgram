aebef2c84dc1cb908984f2226eb86c320c8f0ded
next2014 <83329265@qq.com>
2019-02-12 01:35:38 +0800

    0212

--- wxapp-little-shelf/server/conf/app.js
+++ wxapp-little-shelf/server/conf/app.js
@@ -1,7 +1,7 @@
 module.exports = {
     appConfig: {
-        appid: '/** your appid **/',
-        secret: '/** your appSecret **/'
+        appid: 'wxf4346269b2e05957',
+        secret: 'd7e9058161714e09aae97c55013e23f1'
     },
     userConfig: {
         credit: 30          // 用户初始积分余额
--- wxapp-little-shelf/server/conf/db.js
+++ wxapp-little-shelf/server/conf/db.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-        host: '/** your database server ip **/',
-        user: '/** your database user **/',
-        password: '/** your database account password **/',
-        database: '/** your database **/',
+        host: '192.168.31.34',
+        user: 'root',
+        password: '123',
+        database: 'wxapp',
         port: 3306  // your database server port, default is 3306
     }
 }
