f4beee87d02f319651cfcd2eca7d7099bebfac2d
next2014 <83329265@qq.com>
2019-05-19 10:37:38 +0800

    0519

--- wxapp-shugui/miniprogram/pages/index/index.js
+++ wxapp-shugui/miniprogram/pages/index/index.js
@@ -1,17 +1,27 @@
+const db = wx.cloud.database()
+const book = db.collection('mybook')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    book_list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var _this = this;
+    db.collection('mybook').get({
+       success: res => {
+          this.setData({
+            book_list:res.data
+          })
+       }
+     })
   },
 
   /**
