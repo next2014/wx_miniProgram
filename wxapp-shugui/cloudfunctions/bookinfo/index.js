8bde164cdaecf6c26df70ab614a23f09f06e316e
liangjialei <liangjialei@liangjialeideMacBook-Pro.local>
2019-05-23 00:39:27 +0800

    0523

--- wxapp-shugui/cloudfunctions/bookinfo/index.js
+++ wxapp-shugui/cloudfunctions/bookinfo/index.js
@@ -2,6 +2,7 @@ var rp = require('request-promise')
 
 exports.main = (event, context) => {
    var res = rp('http://api.douban.com/v2/book/isbn/'+ event.isbn).then( html => {
+      // var res = rp('http://api.douban.com/v2/book/isbn/'+ event.isbn).then( html => {
   //  var res = rp('https://douban.uieee.com/v2/movie/subject/' + event.isbn).then(html => {
       return html;
    }).catch( err => {
