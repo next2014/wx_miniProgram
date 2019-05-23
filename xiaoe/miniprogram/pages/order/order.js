cba063f7e1899b21e8e5513c5f7ce3e0ccceac1a
liangjialei <liangjialei@liangjialeideMacBook-Pro.local>
2019-05-24 02:40:16 +0800

    0524

---
+++ xiaoe/miniprogram/pages/order/order.js
@@ -0,0 +1,75 @@
+// pages/order/order.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: [{
+      price: '¥49.80',
+      num: '2',
+      desc: '布伦南一家刚刚从澳大利亚南部的塔斯马尼亚搬到北部的莫维伦巴。',
+      title: '悠长的告别'
+    }],
+    value:''
+  },
+  onChange(event) {
+    // event.detail 为当前输入的值
+    console.log(event.detail);
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
