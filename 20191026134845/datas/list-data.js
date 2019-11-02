44e57ef316e8ed6d766c2ee2b2d01c6a372269c9
next2014 <83329265@qq.com>
2019-11-03 00:53:38 +0800

    1103

---
+++ 20191026134845/datas/list-data.js
@@ -0,0 +1,17 @@
+let list_data = [
+  {
+    images: '1_08_07',
+    type: '平日',
+    title: '园区大门票',
+    price: '20.00'
+  },
+  {
+    images: '1_08_09',
+    type: '成人',
+    title: '海洋世界',
+    price: '78.00'
+  }
+];
+module.exports = {
+  list_data
+};
